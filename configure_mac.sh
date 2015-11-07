fbda30e66664750c092fa54f14b571a6e22d5e5a
Danny Guo <dannyguo91@gmail.com>
2015-11-07 15:25:08 -0500

    Update Mac configuration

--- configure_mac.sh
+++ configure_mac.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-brew tap caskroom/cask
-brew install brew-cask
-brew cask install virtualbox
 brew install ruby python python3 node git tmux vim boot2docker gettext
+brew install the_silver_searcher
+brew install caskroom/cask/brew-cask
+brew cask install docker-compose
+brew cask install dropbox
+brew cask install evernote
+brew cask install java android-studio
+brew cask install karabiner
+brew cask install flux
+brew cask install lastpass
+brew cask install rdio
+brew cask install spectacle
+brew cask install spotify
+brew cask install vlc
+brew cask install virtualbox
 
 # Version of pip that comes with brew is outdated
 pip install --upgrade pip setuptools
 pip3 install --upgrade pip setuptools
-
-npm install -g bower gulp jshint
 
 # Symlink configurations
 ln -s ~/Code/dotfiles/sublime_text/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
