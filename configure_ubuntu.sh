6cc4ba6fc5a98348671ed6b7c0850050ddc4c038
Danny Guo <dannyguo91@gmail.com>
2017-12-12 21:42:41 -0500

    Update Docker installation for Ubuntu

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -28,10 +28,18 @@ fi
 sudo apt-get -y install default-jre
 
 # Docker
-sudo apt-get -y install apt-transport-https ca-certificate
-sudo apt-get -y install linux-image-extra-"$(uname -r)"
-sudo apt-get -y install apparmor
-sudo apt-get -y install docker-engine
+sudo apt-get -y install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 
 # Symlink configurations
 ln -sf ~/Code/dguo/dotfiles/.bash_profile ~/.bash_profile
