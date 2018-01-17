88195f663b615fcba6e05407128e10071df8ff0d
Danny Guo <dannyguo91@gmail.com>
2018-01-17 19:12:11 +0000

    Add remote development setup

---
+++ configure/ubuntu-dev.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DOTFILES=~/code/dguo/dotfiles
+
+# Bash
+ln -sf $DOTFILES/.bash_profile ~/.bash_profile
+ln -sf $DOTFILES/.bashrc ~/.bashrc
+
+# Git
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get install git -y
+ln -sf $DOTFILES/git/ubuntu-dev.gitconfig ~/.gitconfig
+ln -sf $DOTFILES/.gitignore ~/.gitignore
+
+# Vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf $DOTFILES/.vimrc ~/.vimrc
+vim +PlugUpdate +qall
+vim +PlugClean! +qall
+
