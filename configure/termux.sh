96de8e4d1e35c34597afecb3d1e823c433ab2c29
Danny Guo <dannyguo91@gmail.com>
2018-07-27 09:47:30 -0400

    Update Termux setup

--- configure/termux.sh
+++ configure/termux.sh
@@ -6,11 +6,34 @@ DOTFILES=~/code/dguo/dotfiles
 
 pkg upgrade
 
+# Create storage symlinks. See https://termux.com/storage.html
+termux-setup-storage
+
 ln -sf $DOTFILES/.bash_profile ~/.bash_profile
 ln -sf $DOTFILES/.bashrc ~/.bashrc
 
 ln -sf $DOTFILES/git/termux.gitconfig ~/.gitconfig
 ln -sf $DOTFILES/.gitignore ~/.gitignore
 
-wget -O "$PREFIX/etc/bash_completion.d/git-prompt.sh" \
+ln -sf $DOTFILES/.tmux.conf ~/.tmux.conf
+
+ln -sf $DOTFILES/.vimrc ~/.vimrc
+
+curl -o "$PREFIX/etc/bash_completion.d/git-prompt.sh" \
 	https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+# Set the font and color scheme
+curl -Lo ~/.termux/font.ttf --create-dirs \
+    https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/fonts/Source-Code-Pro.ttf
+curl -Lo ~/.termux/colors.properties --create-dirs \
+    https://raw.githubusercontent.com/termux/termux-styling/master/app/src/main/assets/colors/gruvbox-dark.properties
+termux reload-settings
+
+# Install tldr
+curl -Lo "$PREFIX/bin/tldr" \
+    https://gitlab.com/pepa65/tldr-bash-client/raw/master/tldr
+chmod +x "$PREFIX/bin/tldr"
+
+# Install vim-plug
+curl -Lo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
