4976816795df4878069902bdd61bc83b5c169605
Danny Guo <dannyguo91@gmail.com>
2021-11-06 18:47:13 -0400

    Use camel case everywhere for the Hammerspoon configuration

--- programs/hammerspoon/init.lua
+++ programs/hammerspoon/init.lua
@@ -257,28 +257,28 @@ hs.audiodevice.watcher.start()
   Thanks to https://gist.github.com/arbelt/b91e1f38a0880afb316dd5b5732759f1
 ]]
 
-send_escape = false
-last_mods = {}
+sendEscape = false
+lastMods = {}
 
-control_key_handler = function()
-  send_escape = false
+controlKeyHandler = function()
+  sendEscape = false
 end
 
-control_key_timer = hs.timer.delayed.new(0.15, control_key_handler)
+controlKeyTimer = hs.timer.delayed.new(0.15, controlKeyHandler)
 
-control_handler = function(evt)
-  local new_mods = evt:getFlags()
-  if last_mods["ctrl"] == new_mods["ctrl"] then
+controlHandler = function(evt)
+  local newMods = evt:getFlags()
+  if lastMods["ctrl"] == newMods["ctrl"] then
     return false
   end
-  if not last_mods["ctrl"] then
-    last_mods = new_mods
-    send_escape = true
-    control_key_timer:start()
+  if not lastMods["ctrl"] then
+    lastMods = newMods
+    sendEscape = true
+    controlKeyTimer:start()
   else
-    last_mods = new_mods
-    control_key_timer:stop()
-    if send_escape then
+    lastMods = newMods
+    controlKeyTimer:stop()
+    if sendEscape then
       return true, {
         hs.eventtap.event.newKeyEvent({}, 'escape', true),
         hs.eventtap.event.newKeyEvent({}, 'escape', false),
@@ -288,23 +288,23 @@ control_handler = function(evt)
   return false
 end
 
-control_tap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, control_handler)
-control_tap:start()
+controlTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, controlHandler)
+controlTap:start()
 
-other_handler = function(evt)
-  send_escape = false
+otherHandler = function(evt)
+  sendEscape = false
   return false
 end
 
-other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, other_handler)
-other_tap:start()
+otherTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, otherHandler)
+otherTap:start()
 
 -- Maximize the screen brightness when switching to AC power.
-power_source = hs.battery.powerSource()
+powerSource = hs.battery.powerSource()
 hs.battery.watcher.new(function ()
-  local current_power_source = hs.battery.powerSource()
-  if power_source ~= "AC Power" and current_power_source == "AC Power" then
+  local currentPowerSource = hs.battery.powerSource()
+  if powerSource ~= "AC Power" and currentPowerSource == "AC Power" then
     hs.brightness.set(100)
   end
-  power_source = current_power_source
+  powerSource = currentPowerSource
 end):start()
